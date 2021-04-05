ae21768101fd4f80ebf4c699a96d31a75cbe5a70
UlStudent <ksergeiii@mail.ru>
2021-04-05 23:57:25 +0300

    redesign

---
+++ AutoCenterKorytoView/WindowPrePurchaseWork.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AutoCenterKorytoView
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowPrePurchaseWork.xaml
+    /// </summary>
+    public partial class WindowPrePurchaseWork : Window
+    {
+        public WindowPrePurchaseWork()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ AutoCenterKorytoView/WindowPrePurchaseWorks.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AutoCenterKorytoView
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowPrePurchaseWorks.xaml
+    /// </summary>
+    public partial class WindowPrePurchaseWorks : Window
+    {
+        public WindowPrePurchaseWorks()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonUpd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonDel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonRef_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
