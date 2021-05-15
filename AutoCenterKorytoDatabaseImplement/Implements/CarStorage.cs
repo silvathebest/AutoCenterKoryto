9aa28d5cfadeef52aac6a494e4909feaabe34924
UlStudent <ksergeiii@mail.ru>
2021-05-15 18:55:45 +0300

    a

--- AutoCenterKorytoDatabaseImplement/Implements/CarStorage.cs
+++ AutoCenterKorytoDatabaseImplement/Implements/CarStorage.cs
@@ -1,10 +1,42 @@
-﻿using System;
+﻿using AutoCenterKorytoBusinessLogic.BindingModels;
+using AutoCenterKorytoBusinessLogic.Interfaces;
+using AutoCenterKorytoBusinessLogic.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace AutoCenterKorytoDatabaseImplement.Implements
 {
-    public class CarStorage
+    public class CarStorage : ICarStorage
     {
+        public void Delete(CarBindingModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CarViewModel GetElement(CarBindingModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CarViewModel> GetFilteredList(CarBindingModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CarViewModel> GetFullList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(CarBindingModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(CarBindingModel model)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- AutoCenterKorytoDatabaseImplement/Implements/ClientWishesStorage.cs
+++ AutoCenterKorytoDatabaseImplement/Implements/ClientWishesStorage.cs
@@ -1,10 +1,42 @@
-﻿using System;
+﻿using AutoCenterKorytoBusinessLogic.BindingModels;
+using AutoCenterKorytoBusinessLogic.Interfaces;
+using AutoCenterKorytoBusinessLogic.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace AutoCenterKorytoDatabaseImplement.Implements
 {
-    public class ClientWishesStorage
+    public class ClientWishesStorage : IClientWisheStorage
     {
+        public void Delete(ClientWisheBindingModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClientWisheViewModel GetElement(ClientWisheBindingModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ClientWisheViewModel> GetFilteredList(ClientWisheBindingModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ClientWisheViewModel> GetFullList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(ClientWisheBindingModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(ClientWisheBindingModel model)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
